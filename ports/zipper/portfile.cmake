77ba39cb7205581947bb6797c1a5cebdcea96737
mal <malcolm.francis@bowrush.com>
2019-05-03 16:19:07 +1000

    updated zipper

--- ports/zipper/portfile.cmake
+++ ports/zipper/portfile.cmake
@@ -11,36 +11,26 @@
 #
 
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/zipper-1.0.0)
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/SLEEK-TOOLS/zipper/archive/1.0.0.tar.gz"
-    FILENAME "zipper-1.0.0.zip"
-    SHA512 4ff6f4aa525744186f1801e503353e639253668b91f1485a697ed6774d63834118b94e9e6e446e5cd136c9fbd54e6db6e8c89fab5d8fa47a984732b2176a3d7e
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH 
+    REPO sebastiandev/zipper
+    REF v1.0.1
+    SHA512 4aa9a837ea49bf229fd2fd422353acf403afb106e3270a7b53b22c045dfb0ac5fd3b56b105ae43f725f3d8338d64f6d70467ba08f6fbe222590bd1886c4b6f2a
 )
-vcpkg_extract_source_archive(${ARCHIVE})
 
-# work around for git submodule 
-vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH_MINIZIP
-    REPO "sebastiandev/minizip"
-    REF "4acd44bf24025b58e9cf6f8545eb42d2ab36d714"
-    HEAD_REF master
-    SHA512 35c6f32f7af3e37435fb91c6f30b2472893b1ee73990607fd1069ed6db2fcd264425bb3b75b9793b3994051fbaea1c198a3ba346c58c923071670f4e3d5d87d9)
+vcpkg_download_distfile(MINIZIP_ARCHIVE
+    URLS "https://github.com/sebastiandev/minizip/archive/0b46a2b4ca317b80bc53594688883f7188ac4d08.tar.gz"
+    FILENAME "minizip.tar.gz"
+    SHA512 c88f7bbbf679830b2046ae295ece783c751cba98dd4cf4c27fd9e7cce05d8b4a5c717a672649a22ee845b140a665d2e18f5dfe30ac71fb226f21d46e6e66b970
+)
 
-file(GLOB MINIZIP_FILES 
-    "${CURRENT_BUILDTREES_DIR}/src/minizip-4acd44bf24025b58e9cf6f8545eb42d2ab36d714/*.h"
-    "${CURRENT_BUILDTREES_DIR}/src/minizip-4acd44bf24025b58e9cf6f8545eb42d2ab36d714/*.c"
-    )
-file(COPY ${MINIZIP_FILES} DESTINATION ${CURRENT_BUILDTREES_DIR}/src/zipper-1.0.0/minizip)    
+file(REMOVE_RECURSE ${SOURCE_PATH}/minizip)
 
-file(MAKE_DIRECTORY ${CURRENT_BUILDTREES_DIR}/src/zipper-1.0.0/minizip/aes)
-file(GLOB MINIZIP_AES_FILES 
-    "${CURRENT_BUILDTREES_DIR}/src/minizip-4acd44bf24025b58e9cf6f8545eb42d2ab36d714/aes/*.h"
-    "${CURRENT_BUILDTREES_DIR}/src/minizip-4acd44bf24025b58e9cf6f8545eb42d2ab36d714/aes/*.c"
-    )
-file(COPY ${MINIZIP_AES_FILES} DESTINATION ${CURRENT_BUILDTREES_DIR}/src/zipper-1.0.0/minizip/aes)    
+vcpkg_extract_source_archive(${MINIZIP_ARCHIVE} ${SOURCE_PATH})
 
-file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/src/minizip-4acd44bf24025b58e9cf6f8545eb42d2ab36d714)
-file(REMOVE ${CURRENT_BUILDTREES_DIR}/src/sebastiandev-minizip-4acd44bf24025b58e9cf6f8545eb42d2ab36d714.tar.gz.extracted)
+file(RENAME ${SOURCE_PATH}/minizip-0b46a2b4ca317b80bc53594688883f7188ac4d08 ${SOURCE_PATH}/minizip)
+
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
     set(ST_BUILD_STATIC ON)
@@ -52,14 +42,8 @@ endif()
     
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
-# Doesnt export anything when built as dll    
-#    OPTIONS -DBUILD_TEST=OFF -DBUILD_TESTING=OFF -DBUILD_STATIC_VERSION=${ST_BUILD_STATIC} -DBUILD_SHARED_VERSION=${ST_BUILD_SHARED}
-    OPTIONS -DBUILD_TEST=OFF -DBUILD_STATIC_VERSION=ON -DBUILD_SHARED_VERSION=OFF
-
-    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
-    # OPTIONS_RELEASE -DOPTIMIZE=1
-    # OPTIONS_DEBUG -DDEBUGGABLE=1
+    PREFER_NINJA 
+    OPTIONS -DBUILD_TEST=OFF
 )
 
 vcpkg_install_cmake()
