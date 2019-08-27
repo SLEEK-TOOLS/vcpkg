0a9514916825a0e92c50061cb8f71a2ba6fe014c
mal <malcolm.francis@bowrush.com>
2019-08-27 11:57:33 +1000

    Revert "[libpqxx] Fixed issues with port and upgraded to 6.4.5"
    This reverts commit a64513d9b4fa5c1c49bdba2ed51da376d3e95beb.

--- ports/libpqxx/portfile.cmake
+++ ports/libpqxx/portfile.cmake
@@ -3,37 +3,23 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO jtv/libpqxx
-    REF 6.4.5
-    SHA512 b6f79c4af93876eaf859626c2deae3b23bd4fa1a438390bc01513ccc48b90565d59588f6977e7475bb7b8dbeb8b1c8ef2a4737d8eb4d15682531fbf78590ed5e
+    REF 6.4.4
+    SHA512 a168dbc7af210c711fa9f0f6e20d9d3abea167d412a642f591b104a109f11f4c262a27b6919340d405400a58baf7bcc663f7d3ec1b4ecd03f0a4b6c2960b5099
     HEAD_REF master
+	PATCHES
+		fix-deprecated-bug.patch
 )
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/config-public-compiler.h.in DESTINATION ${SOURCE_PATH})
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/config-internal-compiler.h.in DESTINATION ${SOURCE_PATH})
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-	OPTIONS
-		-DBUILD_TEST=OFF
 )
 
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
-
-file(INSTALL ${CURRENT_PACKAGES_DIR}/lib/cmake/libpqxx/libpqxx-config-version.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/libpqxx)
-file(INSTALL ${CURRENT_PACKAGES_DIR}/lib/cmake/libpqxx/libpqxx-config.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/libpqxx)
-file(INSTALL ${CURRENT_PACKAGES_DIR}/lib/cmake/libpqxx/libpqxx-targets-release.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/libpqxx)
-file(INSTALL ${CURRENT_PACKAGES_DIR}/lib/cmake/libpqxx/libpqxx-targets.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/libpqxx)
-file(INSTALL ${CURRENT_PACKAGES_DIR}/debug/lib/cmake/libpqxx/libpqxx-config-version.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/debug/share/libpqxx)
-file(INSTALL ${CURRENT_PACKAGES_DIR}/debug/lib/cmake/libpqxx/libpqxx-config.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/debug/share/libpqxx)
-file(INSTALL ${CURRENT_PACKAGES_DIR}/debug/lib/cmake/libpqxx/libpqxx-targets-debug.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/debug/share/libpqxx)
-file(INSTALL ${CURRENT_PACKAGES_DIR}/debug/lib/cmake/libpqxx/libpqxx-targets.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/debug/share/libpqxx)
-
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/libpqxx)
-
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/cmake)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/cmake)
-
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share/)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/pqxx/doc)
 
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/libpqxx RENAME copyright)
